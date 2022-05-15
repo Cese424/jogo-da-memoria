e54c3d2f27411c29433169ba6f7d227f7098ebb5
Cese424 <alexandresemiatzh@gmail.com>
2022-05-15 13:35:16 -0300

    massive update lol

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Made by Alexandre ;)
 
-//onload basic things
-document.getElementById("winner").style.visibility = "hidden"
-document.getElementById("lost").style.visibility = "hidden"
-
 //array shuffler
 function shuffle(array) {
     let currentIndex = array.length,
@@ -39,9 +35,6 @@ function color_check(z) {
     return document.getElementById("b" + z).style.backgroundColor
 }
 
-function button_color(x) {
-    document.getElementById("b" + x).style.backgroundColor = color_array[x]
-}
 
 function visibility(x) {
     return document.getElementById("b" + x).style.visibility;
@@ -55,12 +48,19 @@ function disable(a) {
 }
 
 //game
+//variaveis
 var round = 0
 var comparador_1
 var comparador_2
 var id_1
 var id_2
-var rodadas_restantes = 10
+var rodadas_restantes = 7
+
+//funções
+function button_color(x) {
+    document.getElementById("b" + x).style.backgroundColor = color_array[x]
+}
+
 
 function button_disable() {
     return disable(1), disable(2), disable(3), disable(4), disable(5), disable(6), disable(7), disable(8), disable(9), disable(10), disable(11), disable(0);
@@ -72,8 +72,9 @@ function rodadas() {
 
 function perder() {
     if (rodadas_restantes == 0) {
-        id("game-box").style.visibility = "hidden"
-        id("lost").style.visibility = "visible"
+        id("game-box").remove()
+        id("winner-loser").src = "img/loser.png"
+        id("winner-loser").style.height = "251px";
     }
 }
 
@@ -85,6 +86,7 @@ function comparador() {
     } else {
         id(id_1).style.backgroundColor = null
         id(id_2).style.backgroundColor = null
+
         rodadas_restantes = rodadas_restantes - 1
         perder()
         rodadas()
@@ -92,8 +94,10 @@ function comparador() {
 }
 
 function ganhador() {
-    if (visibility(1) == "hidden" && visibility(2) == "hidden" && visibility(3) == "hidden" && visibility(4) == "hidden" && visibility(5) == "hidden" && visibility(6) == "hidden" && visibility(7) == "hidden" && visibility(8) == "hidden" && visibility(9) == "hidden" && visibility(10) == "hidden" && visibility(11) == "hidden" && visibility(0) == "hidden") {
-        return document.getElementById("winner").style.visibility = "visible", document.getElementById("game-box").style.visibility = "hidden";
+    if (visibility(1) == "hidden" && visibility(2) == "hidden" && visibility(3) == "hidden" && visibility(4) == "hidden" && visibility(5) == "hidden" && visibility(6) == "hidden" && visibility(7) == "hidden" && visibility(8) == "hidden" && visibility(9) == "hidden" && visibility(10) == "hidden" && visibility(11) == "hidden" && visibility(0) == "hidden" && rodadas_restantes == 7) {
+        id("game-box").remove() = "hidden", id("winner-loser").src = "img/perfect.png", id("winner-loser").style.height = "233px";
+    } else if (visibility(1) == "hidden" && visibility(2) == "hidden" && visibility(3) == "hidden" && visibility(4) == "hidden" && visibility(5) == "hidden" && visibility(6) == "hidden" && visibility(7) == "hidden" && visibility(8) == "hidden" && visibility(9) == "hidden" && visibility(10) == "hidden" && visibility(11) == "hidden" && visibility(0) == "hidden") {
+        id("game-box").remove(), id("winner-loser").src = "img/winner.png", id("winner-loser").style.height = "229px";
     }
 }
 
@@ -103,7 +107,7 @@ function game(a) {
         comparador_1 = color_check(a)
         id_1 = "b" + a
         round = 1
-    } else if (round == 1) {
+    } else if (round == 1 && "b" + a != id_1) {
         button_color(a)
         comparador_2 = color_check(a)
         id_2 = "b" + a
